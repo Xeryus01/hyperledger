f6c65a769be2fe257ad3451dc121eff728455b69
Xeryus01 <akhmadfadilm@gmail.com>
2022-03-03 15:13:55 +0000

    add recover channel org1

---
+++ FabricMultiHostDeployment/setup1/vm1/recoverChannel.sh
@@ -0,0 +1,29 @@
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=${PWD}/../../artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export PEER0_ORG1_CA=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export FABRIC_CFG_PATH=${PWD}/../../artifacts/channel/config/
+
+export CHANNEL_NAME=mychannel
+
+# ganti ORD_IP (IP Orderer) sesuai dengan IP pada VM orderer yang digunakan
+export ORD_IP=34.101.101.106
+export ORD_PORT=7050
+
+# Fungsi untuk export variable sesuai dengan Peer0Org2
+setGlobalsForPeer0Org1(){
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+}
+
+# Fungsi untuk bergabung dengan channel
+joinChannel(){
+    setGlobalsForPeer0Org1
+
+    peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    
+}
+
+# Eksekusi semua fungsi di atas
+joinChannel
