7be9e0da33cd7846b5de2a90eb2d5a7a2a0d21f0
Xeryus01 <akhmadfadilm@gmail.com>
2022-03-09 23:45:13 +0000

    change ord_ip

--- FabricMultiHostDeployment/setup1/vm3/joinChannel.sh
+++ FabricMultiHostDeployment/setup1/vm3/joinChannel.sh
@@ -6,7 +6,7 @@ export FABRIC_CFG_PATH=${PWD}/../../artifacts/channel/config/
 export CHANNEL_NAME=mychannel
 
 # ganti ORD_IP (IP Orderer) sesuai dengan IP pada VM orderer yang digunakan
-export ORD_IP=localhost
+export ORD_IP=34.101.101.106
 export ORD_PORT=7050
 
 # Fungsi untuk export variable sesuai dengan Peer0Org3
