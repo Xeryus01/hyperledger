4e2a442293080855cb2a8a692f113c30c11b0ce5
Xeryus <akhmadfadilm@gmail.com>
2022-03-21 20:37:09 +0700

    add upgradeChaincode

---
+++ FabricMultiHostDeployment/setup1/vm1/upgradeChaincode.sh
@@ -0,0 +1,88 @@
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=${PWD}/../../artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export PEER0_ORG1_CA=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export PEER0_ORG2_CA=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export PEER0_ORG3_CA=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export FABRIC_CFG_PATH=${PWD}/../../artifacts/channel/config/
+
+export CHANNEL_NAME=mychannel
+
+# ganti ORD_IP (IP Orderer) sesuai dengan IP pada VM orderer yang digunakan
+export ORD_IP=34.101.41.164
+export ORD_PORT=7050
+
+# Fungsi untuk export variable sesuai dengan Peer0Org1
+setGlobalsForPeer0Org1() {
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+}
+
+CHANNEL_NAME="mychannel"
+
+# CC_RUNTIME_LANGUAGE diset sebagai node karena peneliti memakai nodejs sebagai bahasa pemrogramannya
+CC_RUNTIME_LANGUAGE=node
+VERSION="2" #setiap upgrade maka version perlu dinaikan 1
+CC_SRC_PATH="./../../artifacts/src/pegawai/"
+CC_NAME="pegawai"
+
+# Fungsi untuk compressing chaincode sehingga dapat diproses lebih lanjut
+packageChaincode() {
+    rm -rf ${CC_NAME}.tar.gz
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode package ${CC_NAME}.tar.gz \
+        --path ${CC_SRC_PATH} --lang ${CC_RUNTIME_LANGUAGE} \
+        --label ${CC_NAME}_${VERSION}
+    echo "===================== Chaincode is packaged on peer0.org1 ===================== "
+}
+
+# Fungsi untuk instalasi chaincode ke Peer0Org1
+installChaincode() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode install ${CC_NAME}.tar.gz
+    echo "===================== Chaincode is installed on peer0.org1 ===================== "
+
+}
+
+# Fungsi untuk melihat chaincode apa saja yang terinstall pada peer tertentu
+queryInstalled() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode queryinstalled >&log.txt
+    cat log.txt
+    PACKAGE_ID=$(sed -n "/${CC_NAME}_${VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
+    echo PackageID is ${PACKAGE_ID}
+    echo "===================== Query installed successful on peer0.org1 on channel ===================== "
+}
+
+# Fungsi untuk menyetujui chaincode pada Org1
+approveForMyOrg1() {
+    setGlobalsForPeer0Org1
+
+    peer lifecycle chaincode approveformyorg -o ${ORD_IP}:${ORD_PORT} \
+        --ordererTLSHostnameOverride orderer.example.com --tls \
+        --signature-policy "OutOf(2, 'Org1MSP.peer', 'Org2MSP.peer', 'Org3MSP.peer')" \
+        --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
+        --init-required --package-id ${PACKAGE_ID} \
+        --sequence ${VERSION}
+
+    echo "===================== chaincode approved from org 1 ===================== "
+
+}
+
+# Fungsi untuk mengecek kesiapan tiap peer anggota channel untuk penggunaan chaincode
+checkCommitReadyness() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode checkcommitreadiness \
+        --signature-policy "OutOf(2, 'Org1MSP.peer', 'Org2MSP.peer', 'Org3MSP.peer')" \
+        --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
+        --sequence ${VERSION} --output json --init-required
+    echo "===================== checking commit readyness from org 1 ===================== "
+}
+
+# Eksekusi semua fungsi di atas
+packageChaincode
+installChaincode
+queryInstalled
+approveForMyOrg1
+checkCommitReadyness
