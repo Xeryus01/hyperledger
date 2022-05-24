bbfc503d20679843fbd1819c64202c184ada25f9
Xeryus01 <akhmadfadilm@gmail.com>
2022-05-24 07:41:45 +0000

    add recover network

--- FabricMultiHostDeployment/setup1/vm3/recoverChaincode.sh
+++ FabricMultiHostDeployment/setup1/vm3/recoverChaincode.sh
@@ -11,11 +11,11 @@ export CHANNEL_NAME=mychannel
 export ORD_IP=157.230.251.53
 export ORD_PORT=7050
 
-# Fungsi untuk export variable sesuai dengan Peer0Org1
-setGlobalsForPeer0Org1() {
-    export CORE_PEER_LOCALMSPID="Org1MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+# Fungsi untuk export variable sesuai dengan Peer0Org3
+setGlobalsForPeer0Org3() {
+    export CORE_PEER_LOCALMSPID="Org3MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG3_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/../../artifacts/channel/crypto-config/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
     export CORE_PEER_ADDRESS=localhost:7051
 
 }
@@ -24,46 +24,46 @@ CHANNEL_NAME="mychannel"
 
 # CC_RUNTIME_LANGUAGE diset sebagai node karena peneliti memakai nodejs sebagai bahasa pemrogramannya
 CC_RUNTIME_LANGUAGE=node
-VERSION="2"
+VERSION="1"
 CC_SRC_PATH="./../../artifacts/src/pegawai/"
 CC_NAME="pegawai"
 
 # Fungsi untuk compressing chaincode sehingga dapat diproses lebih lanjut
 packageChaincode() {
     rm -rf ${CC_NAME}.tar.gz
-    setGlobalsForPeer0Org1
+    setGlobalsForPeer0Org3
     peer lifecycle chaincode package ${CC_NAME}.tar.gz \
         --path ${CC_SRC_PATH} --lang ${CC_RUNTIME_LANGUAGE} \
         --label ${CC_NAME}_${VERSION}
-    echo "===================== Chaincode is packaged on peer0.org1 ===================== "
+    echo "===================== Chaincode is packaged on peer0.org3 ===================== "
 }
 
-# Fungsi untuk instalasi chaincode ke Peer0Org1
+# Fungsi untuk instalasi chaincode ke Peer0Org3
 installChaincode() {
-    setGlobalsForPeer0Org1
+    setGlobalsForPeer0Org3
     peer lifecycle chaincode install ${CC_NAME}.tar.gz
-    echo "===================== Chaincode is installed on peer0.org1 ===================== "
+    echo "===================== Chaincode is installed on peer0.org3 ===================== "
 
 }
 
 # Fungsi untuk melihat chaincode apa saja yang terinstall pada peer tertentu
 queryInstalled() {
-    setGlobalsForPeer0Org1
+    setGlobalsForPeer0Org3
     peer lifecycle chaincode queryinstalled >&log.txt
 
     cat log.txt
     PACKAGE_ID=$(sed -n "/${CC_NAME}_${VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
     echo PackageID is ${PACKAGE_ID}
-    echo "===================== Query installed successful on peer0.org1 on channel ===================== "
+    echo "===================== Query installed successful on peer0.org3 on channel ===================== "
 }
 
-# Fungsi untuk menyetujui chaincode pada Org1
-approveForMyOrg1() {
-    setGlobalsForPeer0Org1
+# Fungsi untuk menyetujui chaincode pada Org3
+approveForMyOrg3() {
+    setGlobalsForPeer0Org3
 
     peer lifecycle chaincode approveformyorg -o ${ORD_IP}:${ORD_PORT} \
         --ordererTLSHostnameOverride orderer.example.com --tls \
-        --signature-policy "OutOf(2, 'Org1MSP.peer', 'Org2MSP.peer', 'Org3MSP.peer')" \
+        --signature-policy "OutOf(2, 'Org3MSP.peer', 'Org2MSP.peer', 'Org3MSP.peer')" \
         --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
         --init-required false --package-id ${PACKAGE_ID} \
         --sequence ${VERSION}
@@ -75,10 +75,10 @@ approveForMyOrg1() {
 # Fungsi untuk mengecek kesiapan tiap peer anggota channel untuk penggunaan chaincode
 checkCommitReadyness() {
 
-    setGlobalsForPeer0Org1
+    setGlobalsForPeer0Org3
     peer lifecycle chaincode checkcommitreadiness --channelID $CHANNEL_NAME \
         --signature-policy "OutOf(2, 'Org1MSP.peer', 'Org2MSP.peer', 'Org3MSP.peer')" \
-        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG3_CA \
         --name ${CC_NAME} --version ${VERSION} --sequence ${VERSION} --output json --init-required
     echo "===================== checking commit readyness from org 2 ===================== "
 }
@@ -86,5 +86,5 @@ checkCommitReadyness() {
 # Eksekusi semua fungsi di atas
 installChaincode
 queryInstalled
-approveForMyOrg1
+approveForMyOrg3
 checkCommitReadyness
